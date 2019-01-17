e3eaae0e615b4213a2c0e41eb4541416d2cd740c
Nick Levi <nick@webt.co.il>
2019-01-17 16:29:38 +0200

    added option to display custom errors

--- dist/js/templates/components/date-picker.hbs.js
+++ dist/js/templates/components/date-picker.hbs.js
@@ -42,12 +42,12 @@ define("ember-date/templates/components/date-picker", ["exports", "ember"], func
           return el0;
         },
         buildRenderNodes: function buildRenderNodes(dom, fragment, contextualElement) {
-          var element7 = dom.childAt(fragment, [3]);
+          var element8 = dom.childAt(fragment, [3]);
           var morphs = new Array(4);
           morphs[0] = dom.createMorphAt(fragment, 1, 1, contextualElement);
-          morphs[1] = dom.createAttrMorph(element7, 'class');
-          morphs[2] = dom.createElementMorph(element7);
-          morphs[3] = dom.createMorphAt(element7, 1, 1);
+          morphs[1] = dom.createAttrMorph(element8, 'class');
+          morphs[2] = dom.createElementMorph(element8);
+          morphs[3] = dom.createMorphAt(element8, 1, 1);
           return morphs;
         },
         statements: [["content", "dateRangeSeparator", ["loc", [null, [6, 2], [6, 24]]]], ["attribute", "class", ["get", "buttonToClasses", ["loc", [null, [7, 18], [7, 33]]]]], ["element", "action", ["toggleOpenTo"], [], ["loc", [null, [7, 36], [7, 61]]]], ["content", "buttonToText", ["loc", [null, [8, 4], [8, 20]]]]],
@@ -93,11 +93,11 @@ define("ember-date/templates/components/date-picker", ["exports", "ember"], func
               return el0;
             },
             buildRenderNodes: function buildRenderNodes(dom, fragment, contextualElement) {
-              var element1 = dom.childAt(fragment, [1]);
+              var element2 = dom.childAt(fragment, [1]);
               var morphs = new Array(3);
-              morphs[0] = dom.createAttrMorph(element1, 'class');
-              morphs[1] = dom.createElementMorph(element1);
-              morphs[2] = dom.createMorphAt(element1, 1, 1);
+              morphs[0] = dom.createAttrMorph(element2, 'class');
+              morphs[1] = dom.createElementMorph(element2);
+              morphs[2] = dom.createMorphAt(element2, 1, 1);
               return morphs;
             },
             statements: [["attribute", "class", ["concat", ["option ", ["get", "option.classes", ["loc", [null, [39, 78], [39, 92]]]]]]], ["element", "action", [["get", "option.action", ["loc", [null, [39, 27], [39, 40]]]], ["get", "option.actionValue", ["loc", [null, [39, 41], [39, 59]]]]], [], ["loc", [null, [39, 18], [39, 61]]]], ["content", "option.label", ["loc", [null, [40, 12], [40, 28]]]]],
@@ -219,19 +219,19 @@ define("ember-date/templates/components/date-picker", ["exports", "ember"], func
           return el0;
         },
         buildRenderNodes: function buildRenderNodes(dom, fragment, contextualElement) {
-          var element2 = dom.childAt(fragment, [1]);
-          var element3 = dom.childAt(element2, [1]);
+          var element3 = dom.childAt(fragment, [1]);
           var element4 = dom.childAt(element3, [1]);
           var element5 = dom.childAt(element4, [1]);
-          var element6 = dom.childAt(element4, [3]);
+          var element6 = dom.childAt(element5, [1]);
+          var element7 = dom.childAt(element5, [3]);
           var morphs = new Array(7);
-          morphs[0] = dom.createAttrMorph(element2, 'class');
-          morphs[1] = dom.createAttrMorph(element2, 'style');
-          morphs[2] = dom.createElementMorph(element5);
-          morphs[3] = dom.createElementMorph(element6);
-          morphs[4] = dom.createMorphAt(dom.childAt(element4, [5]), 0, 0);
-          morphs[5] = dom.createMorphAt(element3, 3, 3);
-          morphs[6] = dom.createMorphAt(element2, 3, 3);
+          morphs[0] = dom.createAttrMorph(element3, 'class');
+          morphs[1] = dom.createAttrMorph(element3, 'style');
+          morphs[2] = dom.createElementMorph(element6);
+          morphs[3] = dom.createElementMorph(element7);
+          morphs[4] = dom.createMorphAt(dom.childAt(element5, [5]), 0, 0);
+          morphs[5] = dom.createMorphAt(element4, 3, 3);
+          morphs[6] = dom.createMorphAt(element3, 3, 3);
           return morphs;
         },
         statements: [["attribute", "class", ["concat", ["date-picker-wrap", ["subexpr", "if", [["get", "options", ["loc", [null, [13, 35], [13, 42]]]], " options"], [], ["loc", [null, [13, 30], [13, 55]]]]]]], ["attribute", "style", ["get", "translateX", ["loc", [null, [13, 65], [13, 75]]]]], ["element", "action", ["gotoPreviousMonth"], [], ["loc", [null, [18, 29], [18, 59]]]], ["element", "action", ["gotoNextMonth"], [], ["loc", [null, [19, 29], [19, 55]]]], ["content", "titleText", ["loc", [null, [21, 27], [21, 40]]]], ["inline", "date-picker-month", [], ["month", ["subexpr", "@mut", [["get", "currentMonth", ["loc", [null, [26, 14], [26, 26]]]]], [], []], "selectedDates", ["subexpr", "@mut", [["get", "selectedDates", ["loc", [null, [27, 22], [27, 35]]]]], [], []], "selectDate", ["subexpr", "action", ["selectDate"], [], ["loc", [null, [28, 19], [28, 40]]]], "minDate", ["subexpr", "@mut", [["get", "minDate", ["loc", [null, [29, 16], [29, 23]]]]], [], []], "maxDate", ["subexpr", "@mut", [["get", "maxDate", ["loc", [null, [30, 16], [30, 23]]]]], [], []], "weekdays", ["subexpr", "@mut", [["get", "weekdays", ["loc", [null, [31, 17], [31, 25]]]]], [], []], "months", ["subexpr", "@mut", [["get", "months", ["loc", [null, [32, 15], [32, 21]]]]], [], []]], ["loc", [null, [25, 6], [33, 8]]]], ["block", "if", [["get", "options", ["loc", [null, [36, 10], [36, 17]]]]], [], 0, null, ["loc", [null, [36, 4], [44, 11]]]]],
@@ -272,12 +272,56 @@ define("ember-date/templates/components/date-picker", ["exports", "ember"], func
           return el0;
         },
         buildRenderNodes: function buildRenderNodes(dom, fragment, contextualElement) {
-          var element0 = dom.childAt(fragment, [1]);
+          var element1 = dom.childAt(fragment, [1]);
           var morphs = new Array(1);
-          morphs[0] = dom.createAttrMorph(element0, 'value');
+          morphs[0] = dom.createAttrMorph(element1, 'value');
           return morphs;
         },
         statements: [["attribute", "value", ["concat", [["get", "value", ["loc", [null, [48, 73], [48, 78]]]]]]]],
+        locals: [],
+        templates: []
+      };
+    })();
+    var child3 = (function () {
+      return {
+        meta: {
+          "revision": "Ember@1.13.11",
+          "loc": {
+            "source": null,
+            "start": {
+              "line": 51,
+              "column": 0
+            },
+            "end": {
+              "line": 53,
+              "column": 0
+            }
+          }
+        },
+        arity: 0,
+        cachedFragment: null,
+        hasRendered: false,
+        buildFragment: function buildFragment(dom) {
+          var el0 = dom.createDocumentFragment();
+          var el1 = dom.createTextNode("  ");
+          dom.appendChild(el0, el1);
+          var el1 = dom.createElement("input");
+          dom.setAttribute(el1, "type", "text");
+          dom.setAttribute(el1, "name", "ember-date-error-date-hidden-input");
+          dom.setAttribute(el1, "value", "");
+          dom.setAttribute(el1, "required", "required");
+          dom.appendChild(el0, el1);
+          var el1 = dom.createTextNode("\n");
+          dom.appendChild(el0, el1);
+          return el0;
+        },
+        buildRenderNodes: function buildRenderNodes(dom, fragment, contextualElement) {
+          var element0 = dom.childAt(fragment, [1]);
+          var morphs = new Array(1);
+          morphs[0] = dom.createAttrMorph(element0, 'data-parsley-required-message');
+          return morphs;
+        },
+        statements: [["attribute", "data-parsley-required-message", ["concat", [["get", "error", ["loc", [null, [52, 105], [52, 110]]]]]]]],
         locals: [],
         templates: []
       };
@@ -292,7 +336,7 @@ define("ember-date/templates/components/date-picker", ["exports", "ember"], func
             "column": 0
           },
           "end": {
-            "line": 49,
+            "line": 53,
             "column": 7
           }
         }
@@ -320,23 +364,28 @@ define("ember-date/templates/components/date-picker", ["exports", "ember"], func
         dom.appendChild(el0, el1);
         var el1 = dom.createComment("");
         dom.appendChild(el0, el1);
+        var el1 = dom.createTextNode("\n");
+        dom.appendChild(el0, el1);
+        var el1 = dom.createComment("");
+        dom.appendChild(el0, el1);
         return el0;
       },
       buildRenderNodes: function buildRenderNodes(dom, fragment, contextualElement) {
-        var element8 = dom.childAt(fragment, [0]);
-        var morphs = new Array(6);
-        morphs[0] = dom.createAttrMorph(element8, 'class');
-        morphs[1] = dom.createElementMorph(element8);
-        morphs[2] = dom.createMorphAt(element8, 1, 1);
+        var element9 = dom.childAt(fragment, [0]);
+        var morphs = new Array(7);
+        morphs[0] = dom.createAttrMorph(element9, 'class');
+        morphs[1] = dom.createElementMorph(element9);
+        morphs[2] = dom.createMorphAt(element9, 1, 1);
         morphs[3] = dom.createMorphAt(fragment, 2, 2, contextualElement);
         morphs[4] = dom.createMorphAt(fragment, 4, 4, contextualElement);
         morphs[5] = dom.createMorphAt(fragment, 5, 5, contextualElement);
+        morphs[6] = dom.createMorphAt(fragment, 7, 7, contextualElement);
         dom.insertBoundary(fragment, null);
         return morphs;
       },
-      statements: [["attribute", "class", ["get", "buttonFromClasses", ["loc", [null, [1, 16], [1, 33]]]]], ["element", "action", ["toggleOpen"], [], ["loc", [null, [1, 36], [1, 59]]]], ["content", "buttonText", ["loc", [null, [2, 2], [2, 16]]]], ["block", "if", [["get", "range", ["loc", [null, [5, 6], [5, 11]]]]], [], 0, null, ["loc", [null, [5, 0], [10, 7]]]], ["block", "if", [["get", "isOpen", ["loc", [null, [12, 6], [12, 12]]]]], [], 1, null, ["loc", [null, [12, 0], [46, 7]]]], ["block", "if", [["get", "isRequired", ["loc", [null, [47, 6], [47, 16]]]]], [], 2, null, ["loc", [null, [47, 0], [49, 7]]]]],
+      statements: [["attribute", "class", ["get", "buttonFromClasses", ["loc", [null, [1, 16], [1, 33]]]]], ["element", "action", ["toggleOpen"], [], ["loc", [null, [1, 36], [1, 59]]]], ["content", "buttonText", ["loc", [null, [2, 2], [2, 16]]]], ["block", "if", [["get", "range", ["loc", [null, [5, 6], [5, 11]]]]], [], 0, null, ["loc", [null, [5, 0], [10, 7]]]], ["block", "if", [["get", "isOpen", ["loc", [null, [12, 6], [12, 12]]]]], [], 1, null, ["loc", [null, [12, 0], [46, 7]]]], ["block", "if", [["get", "isRequired", ["loc", [null, [47, 6], [47, 16]]]]], [], 2, null, ["loc", [null, [47, 0], [49, 7]]]], ["block", "if", [["get", "error", ["loc", [null, [51, 6], [51, 11]]]]], [], 3, null, ["loc", [null, [51, 0], [53, 7]]]]],
       locals: [],
-      templates: [child0, child1, child2]
+      templates: [child0, child1, child2, child3]
     };
   })());
 });
